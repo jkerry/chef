f80373aa6dece62b090f9cb1989397148a93d760
John Kerry <jk185160@ncr.com>
2016-06-03 01:27:06 -0400

    Adding a user rights resource to manage windows rights

---
+++ spec/unit/resource/user_rights_spec.rb
@@ -0,0 +1,29 @@
+#
+# Author:: John Kerry (<john@kerryhouse.net>)
+# Copyright:: Copyright 2008-2016, Chef Software Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require "spec_helper"
+
+describe Chef::Resource::UserRights, "initialize" do
+  before(:each) do
+    @resource = Chef::Resource::UserRights.new("cthulhu")
+  end
+  it "should create a new Chef::Resource::UserRights" do
+    expect(@resource).to be_a_kind_of(Chef::Resource)
+    expect(@resource).to be_a_kind_of(Chef::Resource::UserRights)
+  end
+end
